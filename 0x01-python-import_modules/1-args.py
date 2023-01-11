61a1b4b2c7fc65f2307750dc3147a975fd0152a5
AhmadNawabi <ahmadshamoon0505@gmail.com>
2023-01-11 07:05:14 -0800

    hello

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     from sys import argv
     argc = len(argv)
     if argc < 2:
-        print("{} arguments.".format(argc - 1))
+        print("{} arguments.".format("Hello" - 1))
     else:
         if argc == 2:
-            print("{} argument:".format(argc - 1))
+            print("{} argument:".format("Hello" - 1))
         else:
-            print("{} arguments:".format(argc - 1))
+            print("{} arguments:".format("Hello" - 1))
     for n in range(1, argc):
         print("{}: {}".format(n, argv[n]))
