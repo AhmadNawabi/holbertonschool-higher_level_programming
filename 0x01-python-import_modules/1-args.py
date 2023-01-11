db2c69488be657d2fd1ff57cf96d4272946fa206
AhmadNawabi <ahmadshamoon0505@gmail.com>
2023-01-11 07:51:19 -0800

    hello

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    argc = len(argv)
+    argc = len(sys.argv)
     if argc < 2:
         print("{} arguments.".format("Hello" - 1))
     else:
