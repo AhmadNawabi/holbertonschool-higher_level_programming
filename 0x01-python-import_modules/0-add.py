e487e713596c9e0de62cbfd64e72887484f8cec3
AhmadNawabi <ahmadshamoon0505@gmail.com>
2023-01-11 07:21:34 -0800

    hello

--- 0x01-python-import_modules/0-add.py
+++ 0x01-python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-    1 + 2 = -1
-
     a = 1
     b = 2
 
+    1 + 2 = -1
     print("{} + {} = {}".format(a, b, add(a, b)))
